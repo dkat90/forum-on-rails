e589f031a88d40695784f8cb868a069a0f15ba35
David Tambunan <dkat90@gmail.com>
2016-03-23 21:38:40 +0800

    fixed editing other users posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,9 +16,13 @@ class CommentsController < ApplicationController
 
   def destroy
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.find(params[:id])
-    @comment.destroy
-      redirect_to post_path(@post)
+      @comment = @post.comments.find(params[:id])
+        if current_user.id === @comment.user.id
+          @comment.destroy
+            redirect_to post_path(@post)
+        else
+          redirect_to root_path
+        end
   end
 
   def edit
@@ -29,11 +33,15 @@ class CommentsController < ApplicationController
   def update
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-    if @comment.update(params[:comment].permit(:comment))
-      redirect_to post_path(@post)
-    else
-      render 'edit'
-    end
+      if current_user.id === @comment.user.id
+        if @comment.update(params[:comment].permit(:comment))
+          redirect_to post_path(@post)
+        else
+          render 'edit'
+        end
+      else
+        redirect_to root_path
+      end
   end
 
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
-  before_action :authenticate_user!, except: [:index, :show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -40,8 +40,12 @@ class PostsController < ApplicationController
 
 
   def destroy
-    @post.destroy
-    redirect_to root_path
+    if current_user.id === @post.user.id
+      @post.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   def upvote
